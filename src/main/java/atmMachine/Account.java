2247d9746b727cbc495e8ef357d8f6dcf5676b2b
osmandgn <oskan001@gmail.com>
2022-11-06 20:16:07 +0300

    new version

--- src/main/java/atmMachine/Account.java
+++ src/main/java/atmMachine/Account.java
@@ -41,5 +41,7 @@ public class Account {
         }else balance -= withdraw;
     }
 
+    /* end */
+
 
 }
--- src/main/java/atmMachine/Login.java
+++ src/main/java/atmMachine/Login.java
@@ -14,6 +14,8 @@ public class Login {
             return true;
         }else {return false;}
 
+        /* end */
+
 
 
 
